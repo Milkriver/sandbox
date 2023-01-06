9fc35580853020a0c6214f03ba26811799cd550e
ChurkinaM <mapav4@yandex.ru>
2023-01-06 18:43:38 +0500

    MinMinMax kata

--- codewars/index,.js
+++ codewars/index,.js
@@ -1,5 +1,15 @@
-function repeatStr (n, s) {
-    return s.repeat(n);
-  }
+function repeatStr(n, s) {
+  return s.repeat(n);
+}
 
-  
+function minMinMax(array) {
+  const sortedArray = array.sort((a, b) => a - b);
+  let minimumAbsent;
+  for (let i = 0; i < sortedArray.length; i++) {
+    if (sortedArray[i + 1] > sortedArray[i] + 1) {
+      minimumAbsent = sortedArray[i] + 1;
+      break;
+    }
+  }
+  return [Math.min(...array), minimumAbsent, Math.max(...array)]
+}
