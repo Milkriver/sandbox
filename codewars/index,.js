6baa00c1c1524485283b1b6374a8de10b37d6dfb
ChurkinaM <mapav4@yandex.ru>
2023-01-29 17:52:51 +0500

    getMovieStarRating

--- codewars/index,.js
+++ codewars/index,.js
@@ -88,3 +88,18 @@ const sortOfferList = (selectedSortOption, offers) => {
       return offers;
   }
 };
+
+export const getMovieStarRating = (rating) => {
+  rating = Number(rating);
+  if (rating < 3) {
+    return 'Bad';
+  } else if (rating < 5) {
+    return 'Normal';
+  } else if (rating < 8) {
+    return 'Good';
+  } else if (rating < 10) {
+    return 'Very Good';
+  } else {
+    return 'Awesome';
+  }
+};
